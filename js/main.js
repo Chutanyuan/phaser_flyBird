15fdc1a96e43cb8af9bb267f4f1e8999e2c4d7ce
you@example.com <you@example.com>
2017-06-12 10:43:59 +0800

    aaa

--- js/main.js
+++ js/main.js
@@ -77,7 +77,7 @@ game.States.play = function () {
         this.playTip = game.add.image(game.width / 2, 300, 'play_tip');
         this.readyText.anchor.setTo(0.5, 0);
         this.playTip.anchor.setTo(0.5, 0);
-        this.hasStarted = false;
+        // this.hasStarted = false;
         game.time.events.loop(900, this.generatePipes, this);
         /**
          * 初始的时候设置 game.time.events 事件停止为 false 在开始的时候 开启事件事件 碰撞检测之后设置 事件为false;
@@ -88,13 +88,13 @@ game.States.play = function () {
     this.update = function () {
         if (!this.hasStarted) return;
         //检测 小鸟和地面的碰撞
-        game.physics.arcade.collide(this.bird, this.ground, this.hitGround, null, this);
-        //检测 小鸟和管道的碰撞
-        game.physics.arcade.overlap(this.bird, this.pipeGroup, this.hitPipe, null, this);
-        //检测 管道组中的存在 分数增加
-        this.pipeGroup.forEachExists(this.checkScore, this);
-        //检测 小鸟飞出屏幕外
-        if(!this.bird.inWorld) this.outOfWorld();
+        // game.physics.arcade.collide(this.bird, this.ground, this.hitGround, null, this);
+        // //检测 小鸟和管道的碰撞
+        // game.physics.arcade.overlap(this.bird, this.pipeGroup, this.hitPipe, null, this);
+        // //检测 管道组中的存在 分数增加
+        // this.pipeGroup.forEachExists(this.checkScore, this);
+        // //检测 小鸟飞出屏幕外
+        // if(!this.bird.inWorld) this.outOfWorld();
     };
     this.generatePipes = function () {
         var gap = 150;//控制的是管道之间的距离
@@ -113,25 +113,25 @@ game.States.play = function () {
         this.gameSpeed = 200;
         this.gameIsOver = false;
         this.hasHitGround = false;
-        this.hasStarted = true;
+        // this.hasStarted = true;
         this.score = 0;
         this.bg.autoScroll(-(this.gameSpeed / 10), 0);
         this.ground.autoScroll(-this.gameSpeed, 0);
-        this.bird.body.gravity.y = 1150;
+        // this.bird.body.gravity.y = 1150;
         this.readyText.destroy();//销毁 this.readyText
         this.playTip.destroy();//销毁 this.playTip
         game.input.onDown.add(this.fly, this);//鼠标点击的时候让小鸟飞
         game.time.events.start();  //事件开启
     };
     this.stopGame = function () {
-        this.bg.stopScroll();
-        this.ground.stopScroll();
-        this.pipeGroup.forEachExists(function (pipe) {
-            pipe.body.velocity.x = 0;
-        }, this);
-        this.bird.animations.stop('fly', 0);
-        game.input.onDown.remove(this.fly, this);//移除点击让小鸟飞的动作，否则会出现死亡后依旧可以点击的bug
-        game.time.events.stop(true);
+        // this.bg.stopScroll();
+        // this.ground.stopScroll();
+        // this.pipeGroup.forEachExists(function (pipe) {
+        //     pipe.body.velocity.x = 0;
+        // }, this);
+        // this.bird.animations.stop('fly', 0);
+        // game.input.onDown.remove(this.fly, this);//移除点击让小鸟飞的动作，否则会出现死亡后依旧可以点击的bug
+        // game.time.events.stop(true);
     };
     this.resetPipe = function (topPipeY, bottomPipeY) {
         var i = 0;
@@ -147,55 +147,55 @@ game.States.play = function () {
         }, this);
         return i == 2;
     };
-    this.hitGround = function () {
-        if (this.hasHitGround) return;
-        console.log('小鸟撞在地面上 游戏结束');
-        this.hasHitGround = true;
-        this.gameOver(true);
-    };
+    // this.hitGround = function () {
+    //     // if (this.hasHitGround) return;
+    //     // console.log('小鸟撞在地面上 游戏结束');
+    //     // this.hasHitGround = true;
+    //     // this.gameOver(true);
+    // };
     this.fly = function () {
-        this.bird.body.velocity.y = -350;
+        // this.bird.body.velocity.y = -350;
     };
-    this.hitPipe = function () {
-        if (this.gameIsOver) return;
-        console.log('小鸟碰撞到管道 游戏结束');
-        this.gameOver();
-    };
-    this.gameOver = function (show_text) {
-        this.gameIsOver = true;
-        this.stopGame();
-        if (show_text) this.showGameOverText();
-    };
-    this.showGameOverText = function () {
-        console.log('score = ' + this.score);
-        this.scoreText.destroy();
-        game.bastPoint = game.bastPoint||0;
-        if (this.score>game.bastPoint) game.bastPoint = this.score;
-        this.gameOverGroup = game.add.group();
-        var gameOverText = this.gameOverGroup.create(game.width/2,0,'game_over');
-        gameOverText.anchor.setTo(0.5,0);
-        var scoreboard = this.gameOverGroup.create(game.width/2,70,'score_board');
-        scoreboard.anchor.setTo(0.5,0);
-        game.add.bitmapText(game.width/2 + 60, 105, 'flappy_font', this.score+'', 20, this.gameOverGroup);
-        game.add.bitmapText(game.width/2 + 60, 153, 'flappy_font', game.bastPoint+'', 20, this.gameOverGroup);
-        var reStart = game.add.button(game.width/2,210,'btn',function () {
-            game.state.start('play');
-        },this,null,null,null,null,this.gameOverGroup);
-        reStart.anchor.setTo(0.5,0);
-        this.gameOverGroup.y = 30;
-    };
+    // this.hitPipe = function () {
+    //     // if (this.gameIsOver) return;
+    //     // console.log('小鸟碰撞到管道 游戏结束');
+    //     // this.gameOver();
+    // };
+    // this.gameOver = function (show_text) {
+    //     this.gameIsOver = true;
+    //     this.stopGame();
+    //     if (show_text) this.showGameOverText();
+    // };
+    // this.showGameOverText = function () {
+    //     console.log('score = ' + this.score);
+    //     this.scoreText.destroy();
+    //     game.bastPoint = game.bastPoint||0;
+    //     if (this.score>game.bastPoint) game.bastPoint = this.score;
+    //     this.gameOverGroup = game.add.group();
+    //     var gameOverText = this.gameOverGroup.create(game.width/2,0,'game_over');
+    //     gameOverText.anchor.setTo(0.5,0);
+    //     var scoreboard = this.gameOverGroup.create(game.width/2,70,'score_board');
+    //     scoreboard.anchor.setTo(0.5,0);
+    //     game.add.bitmapText(game.width/2 + 60, 105, 'flappy_font', this.score+'', 20, this.gameOverGroup);
+    //     game.add.bitmapText(game.width/2 + 60, 153, 'flappy_font', game.bastPoint+'', 20, this.gameOverGroup);
+    //     var reStart = game.add.button(game.width/2,210,'btn',function () {
+    //         game.state.start('play');
+    //     },this,null,null,null,null,this.gameOverGroup);
+    //     reStart.anchor.setTo(0.5,0);
+    //     this.gameOverGroup.y = 30;
+    // };
     //检查分数
-    this.checkScore = function (pipe) {
-        if (!pipe.hasScored && pipe.y <= 0 && pipe.x < this.bird.x - 17 - 54) {
-            pipe.hasScored = true;
-            this.scoreText.text = ++this.score;
-            return true;
-        }
-        return false;
-    };
-    this.outOfWorld = function () {
-        this.gameOver();
-    };
+    // this.checkScore = function (pipe) {
+    //     if (!pipe.hasScored && pipe.y <= 0 && pipe.x < this.bird.x - 17 - 54) {
+    //         pipe.hasScored = true;
+    //         this.scoreText.text = ++this.score;
+    //         return true;
+    //     }
+    //     return false;
+    // };
+    // this.outOfWorld = function () {
+    //     this.gameOver();
+    // };
 };
 
 game.state.add('boot', game.States.boot);
